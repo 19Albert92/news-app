9529f9714100f65a764a2a04af9de7b1225b5c31
innurov1992Albert <albert.zinnurov2017@yandex.ru>
2024-02-07 16:44:55 +0100

    add latest news

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,39 +1,15 @@
 import styles from './styles.module.css';
-import {useState} from "react";
-import {getNews} from "@/api/apiNews.js";
-import {PAGE_SIZE} from "@/constants.js";
-import {useFetch} from "@/helpers/hooks/useFetch.js";
 import LatestNews from "@/components/LatestNews/LatestNews.jsx";
 import NewsByFilters from "@/components/NewsByFilters/NewsByFilters.jsx";
 
 const Main = () => {
 
-    const [filter, setFilter] = useState({
-        currentPage: 0,
-        category: 'All',
-        keywords: ''
-    })
-
-    const changeFilter = (field, value) => {
-        setFilter(prev => ({
-            ...prev,
-            [field]: value
-        }))
-    }
-
-    const {data, error, isLoading} = useFetch(getNews, {
-        page_number: filter.currentPage,
-        page_size: PAGE_SIZE,
-        category: filter.category === 'All' ? null : filter.category,
-        keywords: filter.keywords
-    })
-
     return (
         <main className={styles.main}>
 
-            <LatestNews banners={data ? data.news : []}/>
+            <LatestNews/>
 
-            <NewsByFilters news={data ? data.news : []} isLoading={isLoading} changeFilter={changeFilter} filter={filter}/>
+            <NewsByFilters/>
 
         </main>
     );
